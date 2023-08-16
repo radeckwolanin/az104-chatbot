da4ab750f9fd37792ad54943ec9c9525cc152454
radeckwolanin <radeckwolanin@gmail.com>
2023-08-16 20:14:49 +0000

    Widgets reorganization

--- main.py
+++ main.py
@@ -48,6 +48,49 @@ if not openai_api_key:
 qa_tab, upload_tab = st.tabs(["Question & Answer", "Upload"])
 
 with qa_tab:
+                    
+    with st.form(key="qa_form"):
+        query = st.text_area("Ask a question about the document")
+        submit = st.form_submit_button("Submit")
+        
+    with st.expander("Advanced Options"):
+        return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
+        show_full_doc = st.checkbox("Show parsed contents of the document")
+        
+    if show_full_doc:
+        with st.expander("Document"):
+            # Hack to get around st.markdown rendering LaTeX
+            st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>", unsafe_allow_html=True)
+
+    if submit:
+        if not is_query_valid(query):
+            st.stop()
+
+        # Output Columns
+        answer_col, sources_col = st.columns(2)
+
+        result = query_folder(
+            folder_index=folder_index,
+            query=query,
+            return_all=return_all_chunks,
+            model=MODEL,
+            openai_api_key=openai_api_key,
+            temperature=0,
+        )
+
+        with answer_col:
+            st.markdown("#### Answer")
+            st.markdown(result.answer)
+
+        with sources_col:
+            st.markdown("#### Sources")
+            for source in result.sources:
+                st.markdown(source.page_content)
+                st.markdown(source.metadata["source"])
+                st.markdown("---")
+        
+
+with upload_tab:    
     try:
         response = requests.get(api_collections)    # Get list of collections in ChromaDB Server
         response.raise_for_status()  # This will raise an exception for HTTP error status codes
@@ -55,8 +98,6 @@ with qa_tab:
         if response.status_code == 200:
             collections = response.json()
             
-            #collections_df = pd.DataFrame(columns=["Source", "Name", "ID", "Metadata"])
-
             # Prepare data for the table
             table_data = []
             for collection in collections:
@@ -64,29 +105,12 @@ with qa_tab:
                 name = collection.get('name', 'N/A')
                 collection_id = collection.get('id', 'N/A')
                 metadata = collection.get('metadata', 'N/A')
-                #checkmark = st.checkbox("", key=collection_id)  # Add a checkbox for each collection
-                #table_data.append([checkmark, name, collection_id, metadata])
                 table_data.append([checked, name, collection_id, metadata])
-                #collections_df = collections_df.append(
-                    #{
-                    #    "Source": False,
-                    #    "Name": collection.get('name', 'N/A')
-                    #    "ID": collection.get('id', 'N/A')
-                    #    "Metadata": collection.get('metadata', 'N/A')
-                    #},
-                    #collection,
-                    #ignore_index=True
-                #)
             
             # Create a DataFrame
             collections_df = pd.DataFrame(table_data, columns=["Source", "Name", "ID", "Metadata"])
-            #collections_df = pd.DataFrame(table_data)
-                
-
-            # Display the table using st.table
-            st.subheader("Available collections:")            
-            #st.table(collections_df)
-            #st.dataframe(collections_df)
+            
+            st.subheader("Available collections:")
             edited_df = st.data_editor(
                 collections_df,
                 column_config={
@@ -113,17 +137,7 @@ with qa_tab:
             st.error('Error')
     except requests.exceptions.RequestException as e:
         st.write('An error occurred:', e)
-                
-    with st.form(key="qa_form"):
-        query = st.text_area("Ask a question about the document")
-        submit = st.form_submit_button("Submit")
         
-    with st.expander("Advanced Options"):
-        return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
-        show_full_doc = st.checkbox("Show parsed contents of the document")
-        
-
-with upload_tab:
     uploaded_file = st.file_uploader(
         "Upload a pdf, docx, or txt file",
         type=["pdf", "docx", "txt"],
@@ -134,62 +148,25 @@ with upload_tab:
             file = read_file(uploaded_file)
         except Exception as e:
             display_file_read_error(e)
+            
+        chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
+            
+        if not is_file_valid(file):
+            st.stop()
+
+        if not is_open_ai_key_valid(openai_api_key):
+            st.stop()
+
+        with st.spinner("Indexing document... This may take a while⏳"):
+            folder_index = embed_files(
+                files=[chunked_file],
+                embedding=EMBEDDING,
+                vector_store=VECTOR_STORE,
+                openai_api_key=openai_api_key,
+            )
     else:
         st.warning("File queue empty.")
         st.stop()
 
-st.stop()
-
-chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
-
-if not is_file_valid(file):
-    st.stop()
-
-if not is_open_ai_key_valid(openai_api_key):
-    st.stop()
 
 
-with st.spinner("Indexing document... This may take a while⏳"):
-    folder_index = embed_files(
-        files=[chunked_file],
-        embedding=EMBEDDING,
-        vector_store=VECTOR_STORE,
-        openai_api_key=openai_api_key,
-    )
-
-
-
-
-
-if show_full_doc:
-    with st.expander("Document"):
-        # Hack to get around st.markdown rendering LaTeX
-        st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>", unsafe_allow_html=True)
-
-
-if submit:
-    if not is_query_valid(query):
-        st.stop()
-
-    # Output Columns
-    answer_col, sources_col = st.columns(2)
-
-    result = query_folder(
-        folder_index=folder_index,
-        query=query,
-        return_all=return_all_chunks,
-        model=MODEL,
-        openai_api_key=openai_api_key,
-        temperature=0,
-    )
-
-    with answer_col:
-        st.markdown("#### Answer")
-        st.markdown(result.answer)
-
-    with sources_col:
-        st.markdown("#### Sources")
-        for source in result.sources:
-            st.markdown(source.page_content)
-            st.markdown(source.metadata["source"])
-            st.markdown("---")
